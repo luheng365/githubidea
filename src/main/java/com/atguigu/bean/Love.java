4174984342784b3166c7f5ac08f31c31e8829f66
luheng <1066021933@qq.com>
2020-09-27 00:30:06 +0800

    今天是个好日子

---
+++ src/main/java/com/atguigu/bean/Love.java
@@ -0,0 +1,13 @@
+package com.atguigu.bean;
+
+/**
+ * @Author: luheng
+ * @Date: 2020/9/27 0:22
+ * @Description:
+ */
+
+public class Love {
+    public static void main(String[] args) {
+        System.out.println("今天是个好日子....");
+    }
+}
